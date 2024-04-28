85aae176e7a71ddef43863de5b68cb66229e36d6
Obluchatel <azamatovartem@gmail.com>
2024-04-28 16:48:06 +0200

    Small bug fix

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import time
 import os
 
+import app_creds
+
 scope = "user-read-currently-playing, user-read-playback-state"
 client_ID = 'your_client_id'
 client_secret = 'your_client_secret'
@@ -10,15 +12,15 @@ redirect_uri = 'http://localhost:8888'
 
 
 def if_file_exists():
-    flag = "a" if os.path.isfile("myLog.txt") else "w"
-    return flag
+    file_flag = "a" if os.path.isfile("myLog.txt") else "w"
+    return file_flag
 
 def create_log_message(data):
     album_name = "Album name: " + data["item"]["album"]["name"]
     artist_name = "Artist name: " + data["item"]["artists"][0]["name"]
     track_name = "Track name: " + data["item"]["name"]
     link_to_track = "Link to track: " + data["item"]["external_urls"]["spotify"]
-    link_to_album = "Album album: " + data["item"]["album"]["external_urls"]["spotify"]
+    link_to_album = "Link to album: " + data["item"]["album"]["external_urls"]["spotify"]
     timestamp = time.ctime(time.time())
     return (timestamp + " | " + track_name + " | " + artist_name + " | " + album_name + " | "
             + link_to_track + " | " + link_to_album)
@@ -35,7 +37,7 @@ def write_log(results, flag, debug=False):
 
 def main():
     sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(client_id=client_ID, client_secret=client_secret, scope=scope,
+        auth_manager=SpotifyOAuth(client_id=app_creds.client_ID, client_secret=app_creds.client_secret, scope=scope,
                                   redirect_uri=redirect_uri))
     flag = if_file_exists()
     while True:
@@ -47,7 +49,7 @@ def main():
         current_position = data["progress_ms"]
         duration_of_track = data["item"]["duration_ms"]
         log_message = create_log_message(data)
-        write_log(log_message, flag, True)
+        write_log(log_message, flag)
         # additional 7 seconds to give Spotify time to update
         time_to_sleep = ((duration_of_track - current_position) / 1000) + 7
         print(f"Sleeping for: {time_to_sleep}")
